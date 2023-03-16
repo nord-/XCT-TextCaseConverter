ea1a72b27c665cde2978154f8024e57ae37f68d5
Rickard Nord <rickard.nord@chargenode.eu>
2023-03-16 20:01:28 +0100

    first

---
+++ GitApp1/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace GitApp1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPageViewModel ViewModel { get; private set; }
+		public MainPage()
+		{
+			InitializeComponent();
+
+			BindingContext = ViewModel = new MainPageViewModel();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			ViewModel.LicensePlate = "ABC123";
+		}
+	}
+}
---
+++ GitApp1/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GitApp1
+{
+	public class MainPageViewModel : INotifyPropertyChanged
+	{
+		private string _licensePlate;
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public string LicensePlate
+		{
+			get => _licensePlate;
+			set
+			{
+				if (value == _licensePlate) return;
+				_licensePlate = value;
+				OnPropertyChanged();
+			}
+		}
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
